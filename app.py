e11d4e6adaa78e7bcb266785d3d9a44ccbdfe44f
Wardmisp <remi.mialonevan@gmail.com>
2025-11-01 18:05:43 +0100

    debug with threading

--- app.py
+++ app.py
@@ -352,10 +352,13 @@ def bingo_number_sender():
     while True:
         print("Envoi de 28 à tous les clients...")
         for game_id, client_queues in list(sse_clients.items()):
+            logging.info(f"Sending number 28 to game {game_id} clients.")
             message = "event: bingo_number\ndata: 28\n\n"
             for q in client_queues:
+                logging.info(f"Putting message to queue for game {game_id}.")
                 try:
                     q.put_nowait(message)
+                    logging.info(f"Message put to queue for queue {q}.")
                 except:
                     pass
         time.sleep(7)
@@ -386,5 +389,3 @@ def bingo_stream(gameId):
                     del sse_clients[gameId]
 
     return Response(generate(), mimetype='text/event-stream')
-if __name__ == '__main__':
-    app.run(debug=True)
